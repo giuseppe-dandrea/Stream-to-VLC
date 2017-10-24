32430ddcfa578cbd7bbcec0cc9f1ec0e7ddff18f
mattiadr <mattiadr96@gmail.com>
2017-10-24 16:29:01 +0200

    Do not auto close first time + Readme

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-## LINUX:
+# Stream to VLC
+
+**Table of Contents**
+
+- [Installation](#)
+	- [LINUX:](#)
+	- [WINDOWS:](#)
+- [Supported Sites:](#)
+- [Troubleshooting](#)
+
+## Installation
+
+### LINUX:
 
 1. Add the Tampermonkey extension on chrome
 2. Download the script from: <https://greasyfork.org/en/scripts/34206-stream-to-vlc>
@@ -7,10 +19,10 @@ x-scheme-handler/vlcs=vlc-stream.desktop
 4. Open "vlc-stream" and change the [username] with the name of your user
 Exec=/home/[username]/.local/share/applications/vlc-stream.sh %u
 5. Copy both "vlc-stream" files to "home/.local/share/applications"
-6. Read the Troubleshooting paragraph
+6. The first time you open a streaming site you will always have to remember choice (if you didn't go see Troubleshooting)
 7. Enjoy your streaming on VLC
 
-## WINDOWS:
+### WINDOWS:
 
 1. Add the Tampermonkey extension on chrome
 2. Download the script from: <https://greasyfork.org/en/scripts/34206-stream-to-vlc>
@@ -19,14 +31,14 @@ Exec=/home/[username]/.local/share/applications/vlc-stream.sh %u
 5. Add three new  keys "shell", "open" and "command" (See the figures if you have problems) 
 6. Change the Default string value of "command" with the path ["C:\Program Files (x86)\StreamToVLC\vlc_stream.bat" "%1"] (Copy also the quotes)
 7. Now copy "vlc_stream.bat" in the path you wrote at the previous step (You have to create the new "StreamToVLC" folder)
-8. Read the Troubleshooting paragraph
+8. The first time you open a streaming site you will always have to remember choice (if you didn't go see Troubleshooting)
 9. Done! 
 
 NB. If the script doesn't work check that the VLC path in the .bat file is the same of your computer. 
 ![alt text](https://github.com/giuseppe-dandrea/Stream-to-VLC/blob/master/img/screen1.png "screen1")
 ![alt text](https://github.com/giuseppe-dandrea/Stream-to-VLC/blob/master/img/screen2.png "screen2")
 
-## SUPPORTED SITES:
+## Supported Sites:
 
 * openload.co 	-- Fully compatible
 * flashx.to	-- Fully compatible
@@ -34,6 +46,6 @@ NB. If the script doesn't work check that the VLC path in the .bat file is the s
 * wstream.video	-- Open the site and press on the video to open VLC
 * turbovid.me 	-- Open the site and press on the video to open VLC
 
-### Troubleshooting:
+### Troubleshooting
 
-If it's the first time you use the script probably the video tab closes before you can authorize xdg-open. Simply go to the script source code (you can find it in the Tampermonkey dashboard) and add "//" at the beginning of this line: "window.close();", then authorize and remember the authorization for xdg-open and uncomment the line.
+If you didn't set always remember you have to go to the script source code (you can find it in the Tampermonkey dashboard) and add "//" at the beginning of this line: "window.close();" (row 72), then authorize and remember the authorization to open VLC and uncomment the line.
--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Openload to VLC(recursion)
+// @name         Stream to VLC
 // @namespace    http://tampermonkey.net/
-// @version      0.6
-// @description  Opens VLC from Openload stream
+// @version      0.7
+// @description  Opens VLC from streaming sites
 // @author       mattiadr, giuseppe-dandrea
 // @match        http*://openload.co/*
 // @match        http*://turbovid.me/*
@@ -11,70 +11,77 @@
 // @match        http*://wstream.video/*
 // @grant        window.close
 // @grant        GM_openInTab
+// @grant        GM_getValue
+// @grant        GM_setValue
 // ==/UserScript==
 
 var url = null;
 
 if (window.location.href.indexOf('openload.co') != -1) {
-    $('#videooverlay').trigger('click');
-    url = $('#olvideo_html5_api')[0].src;
+	$('#videooverlay').trigger('click');
+	url = $('#olvideo_html5_api')[0].src;
 }
 
 else if (window.location.href.indexOf('turbovid.me') != -1) {
-    $('html').removeClass(" js ");
-    if ($('#video-content').size() === 0) {
-        $('#container > h2').append("<h1 style=\"color:red;\">WAIT AND CLICK ON BUTTON</h1>");
-        //new Promise((resolve) => setTimeout(resolve, 9000)).then(() => {
-        //    $('#btn_download').trigger('click');
-        //});
-    } else {
-        $('#content > h2').append("<h1 style=\"color:red;\">CLICK ON VIDEO TO START VLC</h1>");
-        wait_until_video_click();
-        }
-    }
+	$('html').removeClass(" js ");
+	if ($('#video-content').size() === 0) {
+		$('#container > h2').append("<h1 style=\"color:red;\">WAIT AND CLICK ON BUTTON</h1>");
+		//new Promise((resolve) => setTimeout(resolve, 9000)).then(() => {
+		//    $('#btn_download').trigger('click');
+		//});
+	} else {
+		$('#content > h2').append("<h1 style=\"color:red;\">CLICK ON VIDEO TO START VLC</h1>");
+		wait_until_video_click();
+		}
+	}
 
 else if (window.location.href.indexOf('flashx.to') != -1) {
-    if ($('video').size() === 0) {
-        $('#main > center > h2').append("<h1 style=\"color:red;\">WAIT, THE BUTTON WILL BE PRESSED AUTOMATICALLY</h1>");
-        new Promise((resolve) => setTimeout(resolve, 6000)).then(() => {
-            $('#btn_download').trigger('click');
-        });
-    } else {
-        new Promise((resolve) => setTimeout(resolve, 2000)).then(() => {
-            url = $('video')[0].src;
-            play_url(url);
-        });
-    }
+	if ($('video').size() === 0) {
+		$('#main > center > h2').append("<h1 style=\"color:red;\">WAIT, THE BUTTON WILL BE PRESSED AUTOMATICALLY</h1>");
+		new Promise((resolve) => setTimeout(resolve, 6000)).then(() => {
+			$('#btn_download').trigger('click');
+		});
+	} else {
+		new Promise((resolve) => setTimeout(resolve, 2000)).then(() => {
+			url = $('video')[0].src;
+			play_url(url);
+		});
+	}
 }
 else if (window.location.href.indexOf('rapidvideo.com') != -1) {
-    url = $('video')[0].src;
+	url = $('video')[0].src;
 }
 
 else if (window.location.href.indexOf('wstream.video') != -1) {
-    $('#video-content > table > tbody > tr > td:nth-child(1) > h3').append("<h1 style=\"color:red;\">CLICK ON VIDEO TO START VLC</h1>");
-    wait_until_video_click();
+	$('#video-content > table > tbody > tr > td:nth-child(1) > h3').append("<h1 style=\"color:red;\">CLICK ON VIDEO TO START VLC</h1>");
+	wait_until_video_click();
 }
 
 
 play_url(url);
 
 function play_url(url) {
-    if (url !== null) {
-        window.open('vlcs:' + url, '_self');
-        new Promise((resolve) => setTimeout(resolve, 200)).then(() => {
-            //Comment the next line to be able  authorize xdg-open
-            window.close();
-        });
-    }
+	if (url !== null) {
+		window.open('vlcs:' + url, '_self');
+        if (GM_getValue("VLC_first_time", true)) {
+            alert("Please always remember choice or you will need to edit source code (see Readme for more info)");
+            GM_setValue("VLC_first_time", false);
+        } else {
+            new Promise((resolve) => setTimeout(resolve, 200)).then(() => {
+                //Comment the next line to be able  authorize xdg-open
+                window.close();
+            });
+        }
+	}
 }
 
 function wait_until_video_click() {
-    new Promise((resolve) => setTimeout(resolve, 1000)).then(() => {
-        if ($('video')[0].src.indexOf('http') != -1) {
-            url = $('video')[0].src;
-            play_url(url);
-        } else {
-              wait_until_video_click();
-        }
-        });
+	new Promise((resolve) => setTimeout(resolve, 1000)).then(() => {
+		if ($('video')[0].src.indexOf('http') != -1) {
+			url = $('video')[0].src;
+			play_url(url);
+		} else {
+			  wait_until_video_click();
+		}
+	});
 }
